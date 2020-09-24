a9759bc207db7d6a44a0ee0f41f220e801b1e59a
fujiwood <k-fujiki@future-n.co.jp>
2020-09-24 16:02:02 +0900

    アクセス制限雛形

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  # 各アクションで権限をチェック。オプションでモデル依存をfalseに。
+  authorize_resource class: false
+
+  # 権限が無いページへアクセス時の例外処理
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url
+  end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,10 +4,16 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    if user && user.admin?
+
+    # ログイン/アウトは全てのユーザーで許可
+    can :manage, :session
+
+    user ||= User.new
+
+    if user.admin?
       can :access, :rails_admin   # grant access to rails_admin
-      can :manage, :all           # allow superadmins to do anything
     end
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
