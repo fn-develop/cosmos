1dfd1158e99f2c07dcc871e1a5372622c687d3dd
fujiwood <k-fujiki@future-n.co.jp>
2020-09-24 16:19:50 +0900

    devise session周り調整

--- app/controllers/auth/sessions_controller.rb
+++ app/controllers/auth/sessions_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Home::SessionsController < Devise::SessionsController
+class Auth::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # deviseのログイン機能を自前のコントローラーで管理
-  devise_for :users, controllers: {
-    sessions: 'home/sessions'
+  devise_for :users, path: '', path_names: {
+    # devise のルーティングをデフォルトから変更する設定
+    sign_in: 'login',
+    sign_out: 'logout',
+  },
+  controllers: {
+    sessions: 'auth/sessions'
   }
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
