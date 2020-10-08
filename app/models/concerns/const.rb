1ee836d1f33975a2a2533a8b023778b9d89d5764
fujiwood <k-fujiki@future-n.co.jp>
2020-10-08 17:44:54 +0900

    BS-326 seedデータ追加

---
+++ app/models/concerns/const.rb
@@ -0,0 +1,11 @@
+module Const
+  module CollectionItem
+    module ItemType
+      TEXT     = 'text'
+      SELECT   = 'select'
+      RADIO    = 'radio'
+      CHECKBOX = 'checkbox'
+      DATE     = 'date'
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# User.create(email: 'admin@examole.com', password: 123456, admin: true )
+Company.delete_all
+ActiveRecord::Base.connection.execute('ALTER TABLE companies AUTO_INCREMENT = 1')
+host_company = Company.create(code: 'host_company', name: 'システム管理会社', enabled: true)
+
+Item.delete_all
+ActiveRecord::Base.connection.execute('ALTER TABLE items AUTO_INCREMENT = 1')
+item_name_kanji          = host_company.items.create(code: 'name_kanji', name: 'ユーザー名')
+item_name_kana           = host_company.items.create(code: 'name_kana', name: 'ユーザー名（カナ）')
+item_phone_number        = host_company.items.create(code: 'phone_number', name: '固定電話番号')
+item_mobile_phone_number = host_company.items.create(code: 'mobile_phone_number', name: '携帯電話番号')
+item_gender              = host_company.items.create(code: 'gender', name: '性別')
+item_birthday            = host_company.items.create(code: 'birthday', name: '生年月日')
+item_postal_code         = host_company.items.create(code: 'postal_code', name: '郵便番号')
+item_prefecture          = host_company.items.create(code: 'prefecture', name: '都道府県')
+item_city                = host_company.items.create(code: 'city', name: '市区町村')
+item_address1            = host_company.items.create(code: 'address1', name: '番地')
+item_address2            = host_company.items.create(code: 'address2', name: '建物名・号室')
+
+Collection.delete_all
+ActiveRecord::Base.connection.execute('ALTER TABLE collections AUTO_INCREMENT = 1')
+collection = host_company.collections.create(code: 'customer', name: '顧客情報', sort_order: 0, enabled: true)
+
+CollectionItem.delete_all
+ActiveRecord::Base.connection.execute('ALTER TABLE collection_items AUTO_INCREMENT = 1')
+[
+  { item: item_name_kanji          , item_type: Const::CollectionItem::ItemType::TEXT, sort_order: 0, enabled: true },
+  { item: item_name_kana           , item_type: Const::CollectionItem::ItemType::TEXT, sort_order: 1, enabled: true },
+  { item: item_phone_number        , item_type: Const::CollectionItem::ItemType::TEXT, sort_order: 2, enabled: true },
+  { item: item_mobile_phone_number , item_type: Const::CollectionItem::ItemType::TEXT, sort_order: 3, enabled: true },
+  { item: item_gender              , item_type: Const::CollectionItem::ItemType::RADIO, sort_order: 4, enabled: true },
+  { item: item_birthday            , item_type: Const::CollectionItem::ItemType::DATE, sort_order: 5, enabled: true },
+  { item: item_postal_code         , item_type: Const::CollectionItem::ItemType::TEXT, sort_order: 6, enabled: true },
+  { item: item_prefecture          , item_type: Const::CollectionItem::ItemType::TEXT, sort_order: 7, enabled: true },
+  { item: item_city                , item_type: Const::CollectionItem::ItemType::TEXT, sort_order: 8, enabled: true },
+  { item: item_address1            , item_type: Const::CollectionItem::ItemType::TEXT, sort_order: 9, enabled: true },
+  { item: item_address2            , item_type: Const::CollectionItem::ItemType::TEXT, sort_order: 10, enabled: true },
+].each { |rel| collection.collection_items.create(rel) }
+
+OptionForCollectionItem.delete_all
+ActiveRecord::Base.connection.execute('ALTER TABLE option_for_collection_items AUTO_INCREMENT = 1')
+binding.pry
+collection_item_gender = item_gender.collection_items.first
+collection_item_gender.option_for_collection_items.create(code: 'woman', name: '女性', sort_order: 0, enabled: true)
+collection_item_gender.option_for_collection_items.create(code: 'man', name: '男性', sort_order: 1, enabled: true)
