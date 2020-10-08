a81356883adb6bae4ddff326c98d5d10ed820129
fujiwood <k-fujiki@future-n.co.jp>
2020-10-08 17:44:30 +0900

    BS-326 会社モデルにバリデーション追加

--- app/models/company.rb
+++ app/models/company.rb
@@ -14,4 +14,7 @@ class Company < ApplicationRecord
   has_many :users, through: :company_users
   has_many :items, dependent: :destroy
   has_many :collections, dependent: :destroy
+
+  validates :code, presence: true, uniqueness: true, length: { in: 2..10 }, format: { with: /\A[a-z]+\z/, message: "英文字のみが使用できます" }
+  validates :name, presence: true, length: { in: 1..50 }
 end
