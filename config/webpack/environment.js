743eef063d84796ade5749149a5273cb8da2587a
fujiwood <k-fujiki@future-n.co.jp>
2020-10-09 16:56:14 +0900

    BS-326 「yarn add bootstrap jquery popper.js」

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
+// ADD START
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery'
+  })
+)
+// ADD END
+
 module.exports = environment
