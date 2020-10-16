f7f076bdd67b40b18cd194676b2823c033eec9f6
fujiwood <k-fujiki@future-n.co.jp>
2020-10-16 17:43:43 +0900

    エラーハンドリング処理追加 404, 500

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
   # 権限が無いページへアクセス時の例外処理
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to '/500.html'
+  rescue_from CanCan::AccessDenied, with: :render_500
+
+  def render_404
+    render file: Rails.root.join('public/404.html'), status: 404, layout: false, content_type: 'text/html'
+  end
+
+  def render_500
+    render file: Rails.root.join('public/500.html'), status: 500, layout: false, content_type: 'text/html'
   end
 end
