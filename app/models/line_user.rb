d10a9217039e44d6d3cf08a3658216124c3fd077
fujiwood <k-fujiki@future-n.co.jp>
2020-10-19 14:14:37 +0900

    LINEモデルトークン取得処理修正2

--- app/models/line_user.rb
+++ app/models/line_user.rb
@@ -25,8 +25,8 @@ class LineUser < ApplicationRecord
   # LINE Developers登録完了後に作成される環境変数の認証
   def self.client
     @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ENV[:LINE_CHANNEL_SECRET]
-      config.channel_token  = ENV[:LINE_CHANNEL_TOKEN]
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token  = ENV["LINE_CHANNEL_TOKEN"]
     }
   end
 end
