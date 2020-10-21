2bfae33fdd8c463d854678856fe5ec4e78ef6378
fujiwood <k-fujiki@future-n.co.jp>
2020-10-21 11:06:55 +0900

    未使用のメソッド削除

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,17 +1,14 @@
 class CustomersController < ApplicationMultiTenantController
-  before_action :set_customer, only: [:show, :edit, :update, :destroy, :new_line_message, :send_line_message]
+  before_action :set_customer, only: [:show, :new_line_message, :send_line_message]
   before_action :company
 
-  # GET /customers
   def index
     @customers = company.customer_users
   end
 
-  # GET /customers/1
   def show
   end
 
-  # GET /customers/new
   def new
     session[:reply_token] = params[:reply_token]
     line_user = company.line_users.find_by(reply_token: params[:reply_token])
@@ -21,10 +18,6 @@ class CustomersController < ApplicationMultiTenantController
     else
       @customer = Customer.new
     end
-  end
-
-  # GET /customers/1/edit
-  def edit
   end
 
   # POST /customers
@@ -45,30 +38,6 @@ class CustomersController < ApplicationMultiTenantController
       redirect_to customer_path(@customer, { company_code: company.code }), notice: "登録が完了しました。"
     else
       render :new
-    end
-  end
-
-  # PATCH/PUT /customers/1
-  # PATCH/PUT /customers/1.json
-  def update
-    respond_to do |format|
-      if @customer.update(customer_params)
-        format.html { redirect_to @customer, notice: 'Customer was successfully updated.' }
-        format.json { render :show, status: :ok, location: @customer }
-      else
-        format.html { render :edit }
-        format.json { render json: @customer.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /customers/1
-  # DELETE /customers/1.json
-  def destroy
-    @customer.destroy
-    respond_to do |format|
-      format.html { redirect_to customers_url, notice: 'Customer was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
