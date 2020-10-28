47e94387dc609412c9f4d601a7bb312f828e9d59
fujiwood <k-fujiki@future-n.co.jp>
2020-10-28 17:42:54 +0900

    登録・編集完了後等の通知モーダルメッセージ

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("@rails/activestorage").start()
 require("channels")
 import 'bootstrap'
 import '../stylesheets/application.scss'
-import "@fortawesome/fontawesome-free/js/all";
+import '@fortawesome/fontawesome-free/js/all';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -20,9 +20,11 @@ import "@fortawesome/fontawesome-free/js/all";
 // ADD
 require("jquery");
 require("admin-lte");
+require("izimodal");
 
 window.$ = jQuery;
 window.jQuery = jQuery;
+window.$.fn.iziModal = iziModal;
 
 const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
