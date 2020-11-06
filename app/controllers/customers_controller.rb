10cae2c8457568713d46b673e1e29d17fc389e1c
fujiwood <k-fujiki@future-n.co.jp>
2020-11-06 17:16:07 +0900

    送信後エラー対応

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -80,11 +80,14 @@ class CustomersController < ApplicationMultiTenantController
   end
 
   def send_line_message
+    @line_message_logs = LineMessageLog.where(user_id: @customer.user_id)
     @line_message = LineMessage.new(line_message_params)
     @line_message.company = company
     if @line_message.valid?
       @line_message.send_text_message
+      @line_message = LineMessage.new()
     end
+
     render 'new_line_message'
   end
 
