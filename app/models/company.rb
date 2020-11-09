4cdd06a82d3e2766a21b02a840c0189a10f21d21
fujiwood <k-fujiki@future-n.co.jp>
2020-11-09 17:00:27 +0900

    月間LINEプッシュメッセージ数表示

--- app/models/company.rb
+++ app/models/company.rb
@@ -18,7 +18,20 @@ class Company < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :collections, dependent: :destroy
   has_many :line_users, dependent: :destroy
+  has_many :line_message_logs
 
   validates :code, presence: true, uniqueness: true, length: { in: 2..10 }, format: { with: /\A[a-z]+\z/, message: "英文字のみが使用できます" }
   validates :name, presence: true, length: { in: 1..50 }
+
+  def get_current_month_push_message_count
+    today = Date.today
+    year = today.year.to_s
+    month = today.month.to_s
+    self.line_message_logs.where(
+      year: year,
+      month: month,
+      code: Const::LineMessage::Code::PUSH,
+      success_or_failure: true,
+    ).size().to_s(:delimited)
+  end
 end
