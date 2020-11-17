63ffca16813b517e5d6dd6bd7b605c78014e445d
fujiwood <k-fujiki@future-n.co.jp>
2020-11-17 15:38:31 +0900

    改行コードを改行タグに変更処理

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def activate_css(controller_name)
     return controller_name == params[:controller] ? ' active' : ''
   end
+
+  # テキストエリア等で入力された改行コードをbrに変換する
+  def br(str)
+    html_escape(str).gsub(/\r\n|\r|\n/, '<br />').html_safe
+  end
 end
