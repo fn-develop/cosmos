f14f66549b0b12521895360acace309ea025b9c5
fujiwood <k-fujiki@future-n.co.jp>
2020-12-23 11:45:20 +0900

    バリデーション調整

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,9 +20,10 @@ class Item < ApplicationRecord
   accepts_nested_attributes_for :collection_items, allow_destroy: true
 
   VALID_CODE_REGEX = /\A[a-z\_]+\z/ # 半角英字とアンダーバー
-  validates :code, presence: true, length: { in: 1..20 }, format: { with: VALID_CODE_REGEX, message: 'は半角英文字と「_」のみが使えます' }
-  validates :sub_code, presence: true
-  validates :name, presence: true, length: { in: 1..20 }
+  validates :code, presence: true, length: { in: 1..20 }, format: { with: VALID_CODE_REGEX, message: 'は半角英文字と「_」のみが使えます' }, uniqueness: { scope: [:company_id, :sub_code] }
+  validates :sub_code, presence: true, length: { in: 1..20 }, uniqueness: { scope: [:company_id, :code] }
+  validates :name, presence: true
+  validates :code, presence: true, uniqueness: { scope: :user }
 
   before_destroy :should_not_destroy_if_collection_items
 
